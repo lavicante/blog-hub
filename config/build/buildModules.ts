5bf06afe964a83a131b3a05a4f9ffb837afb04ec
adzhimambetov_ks <demirelkd0696@gmail.com>
2022-09-12 22:07:16 +0300

    add scss (lesson 4)

--- config/build/buildModules.ts
+++ config/build/buildModules.ts
@@ -1,16 +1,29 @@
 import webpack from "webpack";
 
-export function BuildModules (): webpack.ModuleOptions {
+export function BuildModules(): webpack.ModuleOptions {
 
-    const tsLoaderRules =  {
+    const stylesLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    }
+
+    const tsLoaderRules = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     }
 
-     return {
-         rules: [
-             tsLoaderRules
-         ]
-     }
+    return {
+        rules: [
+            tsLoaderRules,
+            stylesLoader
+        ]
+    }
 }
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import {some} from './some'
-
-console.log(some(5, 2))
-
-document.body.innerHTML = '<h1>Hello world</h1>'
