29ba5bee2b6ed9649f2ac57625cc3e6bd3cfd6a6
demirel <demirelkd0696@gmail.com>
2022-10-09 12:02:46 +0300

    add lint stage

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  '*.{js,ts,jsx,tsx}': ['npm run lint:fix', 'npm run prettier:code'],
-  '*.scss': ['lint:scss:fix', 'npm run prettier:code'],
-  '*.json': ['npm run prettier:json'],
-  '*.graphql': ['npm run prettier:gql'],
+  '*.{js,ts,jsx,tsx}': ['npm run lint:fix'],
+  '*.scss': ['lint:scss:fix'],
 };
