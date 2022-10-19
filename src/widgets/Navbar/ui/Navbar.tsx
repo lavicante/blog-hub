b87c1a109a5b0d1e484333bea55957cf2723ba50
adzhimambetov_ks <demirelkd0696@gmail.com>
2022-10-19 23:10:27 +0300

    fix translate

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { AppLInk, AppLinkTheme } from 'shared/AppLink/ui/AppLInk';
 import { classNames } from 'shared/lib/classNames';
 
@@ -8,15 +9,18 @@ interface NavbarProps {
   className?: string;
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-  <div className={classNames(classes.Navbar, [className])}>
-    <div className={classes.Links}>
-      <AppLInk to='/' theme={AppLinkTheme.SECONDARY}>
-        Главная
-      </AppLInk>
-      <AppLInk to='/about' theme={AppLinkTheme.SECONDARY}>
-        О нас
-      </AppLInk>
+export const Navbar = ({ className }: NavbarProps) => {
+  const { t } = useTranslation();
+  return (
+    <div className={classNames(classes.Navbar, [className])}>
+      <div className={classes.Links}>
+        <AppLInk to='/' theme={AppLinkTheme.SECONDARY}>
+          {t('Главная')}
+        </AppLInk>
+        <AppLInk to='/about' theme={AppLinkTheme.SECONDARY}>
+          {t('О нас')}
+        </AppLInk>
+      </div>
     </div>
-  </div>
-);
+  );
+};
