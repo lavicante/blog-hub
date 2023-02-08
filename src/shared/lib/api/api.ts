320a6b914de31551cca0e417bc15831c94b1b220
adzhimambetov_ks <demirelkd0696@gmail.com>
2023-02-08 21:30:04 +0300

    production

--- src/shared/lib/api/api.ts
+++ src/shared/lib/api/api.ts
@@ -1,9 +1,15 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { USER_LOCAL_STORAGE_KEY } from 'shared/constants/localSotorage';
 
 export const $api = axios.create({
   baseURL: __API__,
-  headers: {
-    authorization: localStorage.getItem(USER_LOCAL_STORAGE_KEY),
-  },
+});
+
+$api.interceptors.request.use((config) => {
+  if (config.headers) {
+    config.headers.authorization =
+      localStorage.getItem(USER_LOCAL_STORAGE_KEY) || '';
+  }
+
+  return config;
 });
