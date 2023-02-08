b6643ecd48b14d165733e8789d5c65536e9ac515
adzhimambetov_ks <demirelkd0696@gmail.com>
2023-02-08 21:21:53 +0300

    production

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,3 +1,4 @@
+import CopyPlugin from 'copy-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
@@ -23,6 +24,10 @@ export function BuildPlugins(
       __IS_DEV__: JSON.stringify(isDev),
       __API__: JSON.stringify(apiUrl),
       __PROJECT__: JSON.stringify(project),
+    }),
+
+    new CopyPlugin({
+      patterns: [{ from: paths.locales, to: paths.buildLocales }],
     }),
   ];
 
